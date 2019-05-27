5d229451aa9a1bf83ced44bf6b41f0c21a41977e
keithliam <keithliamm@gmail.com>
2019-05-27 21:00:13 +0800

    Remove unused import

--- learn_kulitan/lib/components/misc/GameLogicManager.dart
+++ learn_kulitan/lib/components/misc/GameLogicManager.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import '../../db/GameData.dart';
 import '../../routes/reading/components.dart' show ChoiceButton;
